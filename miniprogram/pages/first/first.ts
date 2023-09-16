450ef90756277d31b06765ed8785ae9123a3460a
WiserZhou <wiserx@qq.com>
2023-09-16 21:09:31 +0800

    second commit

---
+++ miniprogram/pages/first/first.ts
@@ -0,0 +1,121 @@
+// pages/first/first.ts
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    //存放轮播图数据的列表
+    swiperList: [],
+    urlImg: [{
+      index: 1,
+      urlImage:
+        'https://k.sinaimg.cn/n/default/1_img/upload/3933d981/799/w1014h585/20230913/4426-84f2d2c5e6fbec4072f420635aa53a84.jpg/w710h400z1l50t1q75138.jpg?by=comos'
+    },
+    {
+      index: 2,
+      urlImage:
+        'https://k.sinaimg.cn/n/default/1_img/upload/3933d981/80/w1024h656/20230913/4da2-d894e74472e704d2b82ad3dcde168101.jpg/w640slw.jpg'
+    }],
+    //存放九宫格数据列表
+    gridList: []
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad() {
+    this.getSwiperList()
+    this.getGridList()
+  },
+  //获取轮播图数据的方法
+  getSwiperList() {
+    wx.request({
+      url: 'https://www.escook.cn/slides',
+      method: 'GET',
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          SwiperList: this.data.urlImg
+        })
+        //console.log(this.data.swiperList)
+      }
+    })
+  },
+  //获取九宫格数据方法
+  getGridList() {
+    wx.request({
+      url: 'https://www.escook.cn/categories',
+      method: 'GET',
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          gridList: [{
+            index: 1,
+            name: "h1"
+          }, {
+            index: 2,
+            name: "h2"
+          },
+          {
+            index: 3,
+            name: "h3"
+          },
+          {
+            index: 4,
+            name: "h4"
+          }]
+        })
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
--- miniprogram/pages/list/list.ts
+++ miniprogram/pages/list/list.ts
@@ -5,14 +5,69 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    userList: [
+      { id: 1, name: '小红' },
+      { id: 2, name: '小白' },
+      { id: 3, name: '小蓝' }
+    ],
+    arr1: ['apple', 'banana', 'pear'],
+    sex: 1,
+    info: 'init data',
+    msgList: [{ msg: 'hello' }, { msg: 'world' }],
+    imgSrc: 'http://www.itheima.com/images/logo.png',
+    randomNum: Math.random().toFixed(2) * 10,
+    count: 0,
+    msg: '你好！'
   },
-
+  getInfo() {
+    wx.request({
+      url: 'https://www.escook.cn/api/get',
+      method: 'GET',
+      data: {
+        name: 'zs',
+        age: 20
+      },
+      success: (res) => {
+        console.log(res.data)
+      }
+    })
+  },
+  postInfo() {
+    wx.request({
+      url: 'https://www.escook.cn/api/post',
+      method: 'POST',
+      data: {
+        name: 'ls',
+        age: 33
+      },
+      success: (res) => {
+        console.log(res.data)
+      }
+    })
+  },
+  //输入框的函数
+  inputHandler(e) {
+    this.setData({
+      msg: e.detail.value
+    })
+    console.log(e.detail.value)
+  },
+  btnTapHandler(e) {
+    console.log(e)
+  },
+  changeCount(e) {
+    this.setData({
+      count: this.data.count + 1
+    })
+    console.log('ok')
+    console.log(e.target.dataset.number)
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad() {
-
+    this.getInfo()
+    this.postInfo()
   },
 
   /**
